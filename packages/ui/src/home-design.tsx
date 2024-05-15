2d59ea9246a9aeefe91e838b02cd5de9750fb288
Manik Sharma <maniksharma.rke@gmail.com>
2024-05-16 01:38:03 +0530

    temp

--- packages/ui/src/home-design.tsx
+++ packages/ui/src/home-design.tsx
@@ -5,9 +5,9 @@ export const HomeDesign = () => {
   return (
     <div>
       <div className="relative mt-28">
-        <div className="flex flex-col gap-2 bg-blue-200 h-[80vh] w-9/12 ml-24 rounded-3xl mt-8 relative">
-          <p className="font-thin text-9xl ml-5 mb-0 text-blue-900 z-40">Fast,<br></br>Safe, Social,<br></br>Payments.</p>
-          <p className="z-50 mt-5 w-1/3 ml-5 font-thin text-2xl text-left text-black">Pay, get paid, grow a business, and more. Join the tens of millions of people on PayZap.</p>
+        <div className="flex flex-col gap-2 bg-blue-900 h-[80vh] w-9/12 ml-24 rounded-3xl mt-8 relative">
+          <p className="font-thin text-9xl ml-5 mb-0 text-blue-200 z-40">Fast,<br></br>Safe, Social,<br></br>Payments.</p>
+          <p className="z-50 mt-5 w-1/3 ml-5 font-thin text-2xl text-left text-white">Pay, get paid, grow a business, and more. Join the tens of millions of people on PayZap.</p>
           <Link href={'/signup'}>
           <button className="border-blue-900 bg-transparent border rounded-full mt-4 px-8 py-2 m-auto ml-4 text-xl hover:bg-blue-900 hover:text-white font-thin">
             Get PayZap
